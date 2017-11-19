b957c4eb5ee8ede09b3c74ac6875d983bb0622a6
Latitude3450\Erin <nern.strudel@gmail.com>
2017-11-19 14:04:34 -0500

    added semifuctional player controller

---
+++ XKCD-Jam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float playerSpeed;
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 dir = Vector3.zero;
+
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            dir.x += 1;
+
+        }
+		else if (Input.GetAxis("Horizontal") < 0)
+        {
+            dir.x -= 1;
+
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            dir.z += 1;
+
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            dir.z -= 1;
+        }
+
+        dir = dir.normalized * playerSpeed;
+        rb.velocity = dir;
+    }
+}
